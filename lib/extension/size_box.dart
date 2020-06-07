ce6f8919749a6ea4215a989f0c10a22d31ce7d69
夏菓 <929755313@qq.com>
2020-06-07 11:40:02 +0800

    add square

--- lib/extension/size_box.dart
+++ lib/extension/size_box.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/widgets.dart';
 
-extension SizedBoxExtension on num {
+extension SizedBoxExtension<T extends num> on T {
   SizedBox get heightBox {
     return SizedBox(height: this.toDouble());
   }
 
   SizedBox get widthBox {
     return SizedBox(width: this.toDouble());
+  }
+
+  SizedBox get square {
+    return SizedBox(
+      width: this.toDouble(),
+      height: this.toDouble(),
+    );
   }
 }
